475b8ce195ba1eb2accf2fe1e9bd115b936a60bd
frdm <frdm@38c53164-1e22-0410-8fb6-e7aaf41ea9c6>
2008-03-18 05:25:04 +0000

    コメント状態の判定部不具合修正 イメージファイル名のケースセンティブ対応

--- cat/src/org/F11/scada/cat/logic/impl/ImagePathCheck.java
+++ cat/src/org/F11/scada/cat/logic/impl/ImagePathCheck.java
@@ -93,7 +93,6 @@ public class ImagePathCheck extends AbstractCheckLogic {
 
 	public void execute(String path, ExecuteTask task) throws IOException {
 		if (isSelected) {
-			Set<String> imageFileNameSet = getImageFileNameSet(path);
 			Formatter out = null;
 			try {
 				out = new Formatter(outFile);
@@ -105,7 +104,7 @@ public class ImagePathCheck extends AbstractCheckLogic {
 					if (task.isCancelled()) {
 						break;
 					}
-					checkFile(file, out, path, imageFileNameSet);
+					checkFile(file, out, path, getImageFileNameSet(path));
 					task.setMsg(toString() + "é¿çsíÜ...");
 					task.setProgress(value++, files.size());
 				}
