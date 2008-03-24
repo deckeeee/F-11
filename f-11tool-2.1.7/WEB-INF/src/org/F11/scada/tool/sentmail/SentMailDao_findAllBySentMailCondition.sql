f2ccdcbaaafba4430f5fe6369a0ef622699849f5
frdm <frdm@38c53164-1e22-0410-8fb6-e7aaf41ea9c6>
2008-03-24 03:00:17 +0000

    ファイル／フォルダを削除

--- f-11tool-2.1.7/WEB-INF/src/org/F11/scada/tool/sentmail/SentMailDao_findAllBySentMailCondition.sql
+++
@@ -1,5 +0,0 @@
-SELECT a.alarm_email_sent_id, a.provider, a.holder, a.sentdate, a.value, p.unit, p.name, m.message
-FROM alarm_email_sent_table a, item_table i, message_table m, point_table p
-WHERE a.provider = i.provider AND a.holder = i.holder
-AND i.message_id = m.message_id AND a.value = m.type AND i.point = p.point
-ORDER BY a.sentdate DESC;
--- f-11tool-2.1.7/WEB-INF/src/org/F11/scada/tool/sound/individual/IndividualDao_updateIndividual.sql
+++
@@ -1,2 +0,0 @@
-UPDATE alarm_individual_setting_table SET type = /*dto.type*/1
-WHERE provider = /*dto.provider*/'P1' AND holder = /*dto.holder*/'H1'
