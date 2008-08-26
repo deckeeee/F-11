aaf0073dfc9e88396152ac94e19b45078c1e17ed
hori <hori@38c53164-1e22-0410-8fb6-e7aaf41ea9c6>
2008-08-26 07:27:39 +0000

    commandに自動印字を追加

---
+++ src/org/F11/scada/server/command/GeppoPrintCommand.java
@@ -0,0 +1,46 @@
+/*
+ * Projrct F-11 - Web SCADA for Java Copyright (C) 2002 Freedom, Inc. All Rights
+ * Reserved. This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version. This program is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details. You should have received a copy of the GNU
+ * General Public License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+package org.F11.scada.server.command;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+
+import org.F11.scada.server.alarm.DataValueChangeEventKey;
+
+/**
+ * 月報印刷用クラスです。
+ */
+public class GeppoPrintCommand extends AbstractPrintCommand {
+	/** スレッドプール実行クラス */
+	private static Executor executor = Executors.newCachedThreadPool();
+
+	public void execute(DataValueChangeEventKey evt) {
+		if (0 < csv_mid.length()) {
+			SimpleDateFormat sf = new SimpleDateFormat(csv_mid);
+			Calendar cal = new GregorianCalendar();
+			cal.add(Calendar.MONTH, -1);
+			csv_mid = sf.format(cal.getTime());
+		}
+
+		try {
+			executor.execute(new ListOutPrintTask(evt, "月報"));
+		} catch (RejectedExecutionException e) {
+		}
+	}
+
+}
