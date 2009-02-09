32462b857f7c03c68864942efc3dbe957998e6dd
frdm <frdm@38c53164-1e22-0410-8fb6-e7aaf41ea9c6>
2009-02-09 07:48:07 +0000

    新トレンドグラフ追加

---
+++ src/org/F11/scada/applet/ngraph/draw/CompositGraphDraw.java
@@ -0,0 +1,117 @@
+/*
+ * Project F-11 - Web SCADA for Java
+ * Copyright (C) 2002-2008 Freedom, Inc. All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
+
+package org.F11.scada.applet.ngraph.draw;
+
+import static java.lang.Math.round;
+
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Insets;
+
+
+import org.F11.scada.applet.ngraph.GraphProperties;
+import org.F11.scada.applet.ngraph.LogData;
+import org.F11.scada.applet.ngraph.SeriesProperties;
+import org.apache.commons.collections.primitives.DoubleIterator;
+import org.apache.commons.collections.primitives.DoubleList;
+
+public class CompositGraphDraw extends AbstractGraphDraw {
+
+	public CompositGraphDraw(GraphProperties properties) {
+		super(properties);
+	}
+
+	public void drawSeries(
+			Graphics g,
+			int currentIndex,
+			LogData[] displayDatas,
+			boolean isAllSpanDisplayMode) {
+		Insets insets = properties.getInsets();
+		long lastDate = displayDatas[currentIndex].getDate().getTime();
+		long startDate = lastDate - properties.getHorizontalLineSpan();
+		for (int i = currentIndex; isDrawing(displayDatas, startDate, i); i++) {
+			LogData logData1 = displayDatas[i + 1];
+			LogData logData2 = displayDatas[i];
+			int x1 = getX(logData1, startDate, lastDate, isAllSpanDisplayMode);
+			int x2 = getX(logData2, startDate, lastDate, isAllSpanDisplayMode);
+			DoubleList values1 = logData1.getValues();
+			DoubleList values2 = logData2.getValues();
+			int seriesIndex = 0;
+			for (DoubleIterator i1 = values1.iterator(), i2 =
+				values2.iterator(); i1.hasNext() && i2.hasNext(); seriesIndex++) {
+				int y1 = getY(i1.next(), seriesIndex);
+				int y2 = getY(i2.next(), seriesIndex);
+				SeriesProperties sp =
+					properties.getSeriesGroup().getSeriesProperties().get(
+						seriesIndex);
+				if (sp.isVisible()) {
+					g.setColor(sp.getColor());
+					g.drawLine((int) x1, y1 + insets.top, x2, y2 + insets.top);
+				}
+			}
+		}
+	}
+
+	private boolean isDrawing(LogData[] displayDatas, long startDate, int i) {
+		return i < displayDatas.length - 1
+			&& startDate <= displayDatas[i + 1].getDate().getTime();
+	}
+
+	private int getY(double value, int seriesIndex) {
+		SeriesProperties sp =
+			properties.getSeriesGroup().getSeriesProperties().get(seriesIndex);
+		float max = sp.getMax();
+		float min = sp.getMin();
+		float r = properties.getVerticalLine() / (max - min);
+		int round = (int) round((max - value) * r);
+		int dispMin = properties.getVerticalLine();
+		return (round <= dispMin && round >= 0) ? round : (round > dispMin)
+			? round(dispMin)
+			: 0;
+	}
+
+	public void drawVerticalString(
+			Graphics g,
+			int top,
+			int x,
+			int y,
+			int i,
+			int drawSeriesIndex) {
+		SeriesProperties p =
+			properties.getSeriesGroup().getSeriesProperties().get(
+				drawSeriesIndex);
+		float max = p.getMax();
+		String dateStr =
+			String.format(p.getVerticalFormat(), max
+				- i
+				* ((max - p.getMin()) / properties.getVerticalCount()));
+		FontMetrics metrics = g.getFontMetrics();
+		if (p.isVisible()) {
+			g.setColor(p.getColor());
+			g.drawString(dateStr, x
+				- metrics.stringWidth(dateStr)
+				- properties.getScalePixcelSize(), round(top
+				+ y
+				+ metrics.getAscent()
+				/ DATE_STRING_RATE));
+		}
+	}
+}
---
+++ src/org/F11/scada/applet/ngraph/model/DefaultGraphModel.java
@@ -0,0 +1,174 @@
+/*
+ * Project F-11 - Web SCADA for Java
+ * Copyright (C) 2002-2008 Freedom, Inc. All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
+
+package org.F11.scada.applet.ngraph.model;
+
+
+import java.rmi.RemoteException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+
+import org.F11.scada.applet.graph.DefaultSelectiveValueListHandlerFactory;
+import org.F11.scada.applet.graph.SelectiveValueListHandlerFactory;
+import org.F11.scada.applet.ngraph.GraphProperties;
+import org.F11.scada.applet.ngraph.LogData;
+import org.F11.scada.applet.ngraph.SeriesGroup;
+import org.F11.scada.applet.ngraph.SeriesProperties;
+import org.F11.scada.server.io.SelectiveValueListHandler;
+import org.F11.scada.server.register.HolderString;
+import org.apache.commons.collections.primitives.DoubleList;
+
+public class DefaultGraphModel extends AbstractGraphModel {
+	private final static int MAX_RECORD = 6000;
+	private final Logger logger = Logger.getLogger(DefaultGraphModel.class.getName());
+	/** データハンドラマネージャーです。 */
+	private SelectiveValueListHandler valueListHandler;
+	/** データ更新スケジュール */
+	private ScheduledExecutorService service;
+
+	public DefaultGraphModel(GraphProperties graphProperties) {
+		super(graphProperties);
+		service = Executors.newScheduledThreadPool(1);
+		SelectiveValueListHandlerFactory factory =
+			new DefaultSelectiveValueListHandlerFactory();
+		valueListHandler = factory.getSelectiveValueListHandler();
+	}
+
+	public int getMaxRecord() {
+		return MAX_RECORD;
+	}
+
+	public void initialize() {
+		List<LogData> logData = getLogData();
+		if (0 < logData.size()) {
+			service.scheduleAtFixedRate(
+				getLastLogData(logData),
+				getInitialDelay(),
+				60,
+				TimeUnit.SECONDS);
+			changeSupport.firePropertyChange(INITIALIZE, null, logData);
+		}
+	}
+
+	private LoggingTask getLastLogData(List<LogData> logData) {
+		return new LoggingTask(logData.get(logData.size() - 1));
+	}
+
+	private long getInitialDelay() {
+		Calendar cal = Calendar.getInstance();
+		int s = cal.get(Calendar.SECOND);
+		return s == 0 ? 5 : 65 - (s % 60);
+	}
+
+	private List<LogData> getLogData() {
+		List<LogData> list = Collections.emptyList();
+		try {
+			SortedMap<Timestamp, DoubleList> map =
+				valueListHandler.getInitialData(
+					getLogName(),
+					getHolderStrings());
+			list = convertLogDataList(map);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	private List<HolderString> getHolderStrings() {
+		SeriesGroup sg = graphProperties.getSeriesGroup();
+		ArrayList<HolderString> list = new ArrayList<HolderString>();
+		for (SeriesProperties seriesProperties : sg.getSeriesProperties()) {
+			list.add(seriesProperties.getHolderString());
+		}
+		return list;
+	}
+
+	private List<LogData> convertLogDataList(
+			SortedMap<Timestamp, DoubleList> map) {
+		ArrayList<LogData> list = new ArrayList<LogData>(map.size());
+		for (Map.Entry<Timestamp, DoubleList> entry : map.entrySet()) {
+			list.add(new LogData(entry.getKey(), entry.getValue()));
+		}
+		return list;
+	}
+
+	public boolean reachedMaximum() {
+		return false;
+	}
+
+	public boolean reachedMinimum() {
+		return false;
+	}
+
+	public void shutdown() {
+		service.shutdown();
+	}
+
+	private class LoggingTask implements Runnable {
+		/** 最新のログデータ */
+		private Timestamp currentTimestamp;
+
+		public LoggingTask(LogData lastLogData) {
+			this.currentTimestamp =
+				new Timestamp(lastLogData.getDate().getTime());
+		}
+
+		public void run() {
+			try {
+				Map<Timestamp, DoubleList> map =
+					valueListHandler.getUpdateLoggingData(
+						getLogName(),
+						currentTimestamp,
+						getHolderStrings());
+				performPropertyChange(map);
+				setCurrentTimestamp(map);
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+		}
+
+		private void performPropertyChange(Map<Timestamp, DoubleList> map) {
+			for (Map.Entry<Timestamp, DoubleList> entry : map.entrySet()) {
+				changeSupport.firePropertyChange(
+					GraphModel.VALUE_CHANGE,
+					null,
+					new LogData(entry.getKey(), entry.getValue()));
+			}
+		}
+
+		private void setCurrentTimestamp(Map<Timestamp, DoubleList> map) {
+			for (Timestamp timestamp : map.keySet()) {
+				if (currentTimestamp.after(timestamp)) {
+					currentTimestamp = timestamp;
+				}
+			}
+		}
+	}
+}
