812f323ee93f524420920d66b5eb913dd31f72ec
frdm <frdm@38c53164-1e22-0410-8fb6-e7aaf41ea9c6>
2014-01-30 01:14:03 +0000

    バージョン情報修正

---
+++ f-11.2.1.31/コピー ～ log_table_weekly.sql
@@ -0,0 +1,177 @@
+-- MySQL dump 9.11
+--
+-- Host: localhost    Database: wifedb
+-- ------------------------------------------------------
+-- Server version	4.0.27-nt
+
+--
+-- Table structure for table `log_table_month`
+--
+
+CREATE TABLE log_table_dailyyearout (
+  f_date datetime NOT NULL default '0000-00-00 00:00:00',
+  f_revision int(11) NOT NULL default '0',
+  f_P1_D_1900000_Digital tinyint(1) default NULL,
+  f_P1_D_500_BcdSingle double default NULL,
+  f_P1_D_501_BcdSingle double default NULL,
+  f_P1_D_502_BcdSingle double default NULL,
+  PRIMARY KEY  (f_date,f_revision)
+) TYPE=MyISAM;
+
+--
+-- Dumping data for table `log_table_month`
+--
+
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-01 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-02 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-03 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-04 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-05 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-06 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-07 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-08 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-09 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-10 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-11 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-12 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-13 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-14 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-15 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-16 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-17 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-18 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-19 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-20 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-21 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-22 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-23 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-24 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-25 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-26 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-27 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-28 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-29 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-30 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-01-31 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-01 00:01:00',0,0,0,0,0);
+
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-02 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-03 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-04 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-05 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-06 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-07 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-08 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-09 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-10 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-11 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-12 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-13 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-14 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-15 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-16 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-17 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-18 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-19 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-20 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-21 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-22 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-23 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-24 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-25 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-26 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-27 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-28 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-02-29 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-01 00:01:00',0,0,0,0,0);
+
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-02 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-03 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-04 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-05 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-06 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-07 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-08 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-09 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-10 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-11 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-12 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-13 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-14 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-15 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-16 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-17 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-18 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-19 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-20 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-21 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-22 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-23 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-24 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-25 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-26 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-27 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-28 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-29 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-30 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-03-31 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-01 00:01:00',0,0,0,0,0);
+
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-02 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-03 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-04 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-05 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-06 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-07 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-08 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-09 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-10 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-11 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-12 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-13 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-14 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-15 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-16 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-17 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-18 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-19 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-20 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-21 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-22 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-23 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-24 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-25 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-26 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-27 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-28 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-29 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-04-30 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-01 00:01:00',0,0,0,0,0);
+
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-02 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-03 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-04 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-05 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-06 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-07 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-08 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-09 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-10 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-11 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-12 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-13 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-14 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-15 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-16 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-17 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-18 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-19 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-20 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-21 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-22 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-23 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-24 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-25 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-26 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-27 00:01:00',0,0,0,0,0);
+INSERT INTO log_table_dailyyearout VALUES ('2012-05-28 00:01:00',0,0,0,0,0);
--- shutdown.bat
+++
@@ -1,2 +0,0 @@
-call setenv.bat
-java -Dsun.nio.cs.map=x-windows-iso2022jp/ISO-2022-JP -Xmx64m -classpath %CL% org.F11.scada.xwife.server.EndCommand %1
