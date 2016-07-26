af8a71e01105fc4e335252ea10e5f208d5db07b0
frdm <frdm@38c53164-1e22-0410-8fb6-e7aaf41ea9c6>
2016-07-26 06:25:29 +0000

    f-11-2.1.34 PlcCommunicaterのsendBufferをフィールド共有から、毎回allocateするように変更 特に必要ないためByteBufferのdirectAllocateを全てallocateに変更

--- f-11-2.1.34/starta.bat
+++ f-11-2.1.34/starta.bat
@@ -1,3 +1,3 @@
 call setenv.bat
-java -agentlib:yjpagent -Xmx128m -Xms128m -Xmn64m -classpath %CL% org.F11.scada.xwife.applet.WifeAppletD %1
+java -Xmx128m -Xms128m -Xmn64m -classpath %CL% org.F11.scada.xwife.applet.WifeAppletD %1
 @rem java -Xmx128m -Xms128m -Xmn64m -classpath %CL% org.F11.scada.xwife.applet.WifeAppletD %1
